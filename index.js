333a93e2ddce8a5d6dceff989d4bc9a20d0328c6
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-04-03 10:49:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much.";
+
+//d4d1108214c82522529348fc35fa0c3a2ded2fcf
+
+
+const token = 'd4d1108214c82522529348fc35fa0c3a2ded2fcf'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
